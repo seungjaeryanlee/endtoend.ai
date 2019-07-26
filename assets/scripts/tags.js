21ddec79d6aa77710c8d1b58173d65eea3ae087d
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2019-07-27 05:33:43 +0900

    Revert "Make tags clickable in blog page"
    This reverts commit 2189e7a4efac250b5c01fae2e4a24700de9eb5ce.

--- assets/scripts/tags.js
+++ assets/scripts/tags.js
@@ -1,6 +1,5 @@
-// Allow clicking tags
 $(document).ready(() => {
-    // Home page: home.html
+    // Home page: index.md
     $('.featured-card__tags__tag').on('click', (e) => {
         let link = $(e.target).attr("goto");
         window.location.replace(link);
@@ -10,24 +9,6 @@ $(document).ready(() => {
     });
     $('.featured-card').on('click', (e) => {
         let link = $('.featured-card').attr("goto");
-        window.location.replace(link);
-    });
-
-    // Blog page: blog.html
-    $('.post__summary__tags__tag').on('click', (e) => {
-        let link = $(e.target).attr("goto");
-        window.location.replace(link);
-        e.cancelBubble = true;
-        if (e.stopPropagation) e.stopPropagation();
-        if (e.preventDefault) e.preventDefault();
-    });
-    $('.post-container .post').on('click', (e) => {
-        // Find the correct .post class surrounding whatever is clicked
-        let block = $(e.target);
-        while (!block.attr("goto")) {
-            block = block.parent()
-        }
-        let link = block.attr("goto");
         window.location.replace(link);
     });
 });
